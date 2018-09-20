ea93eff9587534328a30c5668a4ed16dbbfc6fbb
Nikita Malov <nikmalov@gmail.com>
2018-09-20 23:01:19 +0200

    Fixed project naming

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 extern "C"
 jstring
-Java_com_ignidev_nik_interviewproject_ui_MainActivity_stringFromJNI(
+Java_com_ignidev_nik_JsonFetcher_ui_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     string hello = "Hello from C++";
